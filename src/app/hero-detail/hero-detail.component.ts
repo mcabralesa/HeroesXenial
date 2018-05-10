796aa4f75b18f36e3fafe0e56d97c1b8e65804a1
PSLCOL\mcabralesa <mcabralesa@psl.com.co>
2018-05-10 18:10:57 -0500

    Hero Detail Test Done 2

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -11,31 +11,15 @@ import {AppState} from "../AppState";
     templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit {
-
     heroes: Observable<Hero[]>;
-    _nickname: string;
-    _name: string;
-    _height: number;
-    _picture: string;
     index: number;
-
     constructor(private route: ActivatedRoute, private store: Store<AppState>) {
-
     }
-
     ngOnInit() {
         this.heroes = this.store.select(state => state.heroes);
         this.route.params.subscribe(params => {
             this.index = +params['index'];
             this.index--;
-            this.heroes.subscribe(response => {
-                this._nickname = response[this.index]._nickname;
-                this._name = response[this.index]._name;
-                this._height = response[this.index]._height;
-                this._picture = response[this.index]._picture;
-
-            });
         });
     }
-
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -15,7 +15,6 @@ export class HeroesComponent implements OnInit {
     }
 
     ngOnInit() {
-        
     }
 
     clickHero() {
